c82a613a358f241687458c1df894a8f772659637
samuelhovington <43546049+samuelhovington@users.noreply.github.com>
2018-09-25 11:13:22 -0400

    Update JacoCommandModes.m
    Adding new modes to cartesian control

--- JACO2Communicator/Source/JacoCommandModes.m
+++ JACO2Communicator/Source/JacoCommandModes.m
@@ -10,6 +10,8 @@ classdef JacoCommandModes < int32
         SEND_POSITION_CMD (2)
         SEND_TORQUE_CMD (3)
         SEND_FINGER_CMD (4)
+        SEND_CART_POSITION_CMD (5)
+        SEND_CART_VELOCITY_CMD (6)
     end
     
     methods
