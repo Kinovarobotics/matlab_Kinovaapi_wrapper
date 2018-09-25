276a49736359e3d8eb705115a7a14f6e06654a18
samuelhovington <43546049+samuelhovington@users.noreply.github.com>
2018-09-25 11:10:47 -0400

    Update compileMexInterface.m
    Adding a change directory to make sure the new JacoMexInterface.mexw64 file always overwrite the old one
    Changing path for

--- JACO2Communicator/Source/compileMexInterface.m
+++ JACO2Communicator/Source/compileMexInterface.m
@@ -9,4 +9,5 @@ ipath2 = '-IC:\Users\mblouin\Desktop\JACO2Communicator\Source';
 lpath = '-LC:\Users\mblouin\Desktop\JACO2Communicator\JACO2SDK';
 mexInterfaceFile = 'C:\Users\mblouin\Desktop\JACO2Communicator\JACO2SDK\JacoMexInterface.cpp';
 wrapperFile = 'C:\Users\mblouin\Desktop\JACO2Communicator\JACO2SDK\JacoSDKWrapper.cpp';
+cd 'C:\Users\mblouin\Desktop\JACO2Communicator\Source';
 mex('-v',ipath1,ipath2,lpath,mexInterfaceFile,wrapperFile); 
